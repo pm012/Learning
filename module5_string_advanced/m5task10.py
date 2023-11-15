c3dc736826503cf8760cf491f98154938fee73a1
Serhii Kroshka <pm012@ukr.net>
2023-11-16 01:30:04 +0200

    fixed errors in task 10

--- module5_string_advanced/m5task10.py
+++ module5_string_advanced/m5task10.py
@@ -17,21 +17,37 @@ Result:
     'string': 'Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.'
 }
 
+In case word is not found:
+print(find_word(
+    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
+    "python"))
 
-
+{
+    'result': False,
+    'first_index': None,
+    'last_index': None,
+    'search_string': 'python',
+    'string': 'Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.'
+}
 
 """
 
 import re
-import json
-
 
 def find_word(text, word):
-    res = re.search(word, text)    
-    return json.dumps({'result': bool(res), 'fist_index': res.span()[0], 'last_index': res.span()[1], 'search_sting': res.string, 'string': res.group()}, indent = 4)
+    res = re.search(word, text)
+    if bool(res):
+          return {'result': bool(res), 'first_index': res.span()[0], 'last_index': res.span()[1], 'search_string': res.group(), 'string': res.string }
+    else:
+         return {'result': bool(res), 'first_index': None, 'last_index': None, 'search_string': word, 'string': text}
+         
 
     
 
 print(find_word(
-    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
+    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",    
     "Python"))
+
+print(find_word(
+    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
+    "python"))
